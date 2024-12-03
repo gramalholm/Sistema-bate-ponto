ff9834d642a096e550b08b87f5c32b40c83e4e61
Gabriel Ramalho <biel.kinb@gmail.com>
2024-12-03 17:44:11 -0300

    controladores e rotas

---
+++ src/api/routes/funcRoutes.ts
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+=======
+import { response, Router } from "express";
+import express, {Request, Response} from 'express';
+import { funcController } from "../controllers/funcController";
+const funcRouter = Router();
+
+funcRouter.get('/func', (req: Request, res: Response) => {
+    
+
+})
+>>>>>>> cca6ca0 (controladores e rotas)
