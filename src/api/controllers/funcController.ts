c48aeafc89aa5122ceaa87451e32480ec9142129
Leonardo de Freitas Nogueira <leonogueira.nardo@gmail.com>
2024-12-14 16:53:15 -0300

    Atualizando o BD

--- src/api/controllers/funcController.ts
+++ src/api/controllers/funcController.ts
@@ -1,3 +1,9 @@
+import express, {Request, Response} from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const app = express();
+const prisma = new PrismaClient()
+
 export class funcController{
     public static async changePassword(req: Request, res: Response):Promise<Response>{
         try{
